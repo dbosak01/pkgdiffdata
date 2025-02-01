1b43b7c098d44e6be69aa064a9263d67e435ab67
David J. Bosak <dbosak01@gmail.com>
2025-02-01 14:29:18 -0500

    Sample Package data

--- R/CreateData.R
+++ R/CreateData.R
@@ -4,27 +4,53 @@ remotes::install_github("https://github.com/dbosak01/pkgdiff")
 
 
 library(pkgdiff)
-library(packageDiff)
+#library(packageDiff)
 
 pkgs <- c("common", "procs", "libr", "defineR", "reporter", "fmtr", "sassy", "logr")
 
 
 
 
-
-
-create_package_data <- function(pkg) {
-
+github_data <- function(pkg, versions = NULL) {
 
 
 
 }
 
+create_package_data <- function(pkg) {
+
+  ret <- list()
+
+  infos <- pkgdiff:::get_all_infos(pkg)
+
+
+  dat <- pkgdiff:::get_info_data(pkg, infos)
+
+
+  ret[["stability"]] <- dat
+  ret[["infos"]] <- infos
+
+  fl <- file.path("data", paste0(pkg, ".rds"))
+
+  save(ret, file = fl)
+
+  return(ret)
+}
+
+# Test
+res <- create_package_data("procs")
 
 
 
-
+# Test vector
 pkgs <- c("common", "procs", "libr", "defineR", "reporter", "fmtr", "sassy", "logr")
+
+for (pkg in pkgs) {
+
+  res <- create_package_data(pkg)
+
+}
+
 
 pkvect <- pkgdiff:::get_latest_version(pkgs)
 
